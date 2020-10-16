690ed48754b9feb4f3ab65e69a6e3a310872be9b
John <jxnata@iMac-de-John.local>
2020-10-16 11:12:14 -0300

    fix category type

--- models/Category.ts
+++ models/Category.ts
@@ -1,9 +1,9 @@
 export default class Catefory {
     name: string
-    type: Array<string>
+    type: string
     image: string
 
-    constructor(name: string, type: Array<string>, image: string) {
+    constructor(name: string, type: string, image: string) {
         this.name = name
         this.type = type
         this.image = image
