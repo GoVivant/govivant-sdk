290e278207b0e6307e896c19b899452c2fd8d0a3
jxnata <jonata.96ol@gmail.com>
2021-04-08 18:41:39 -0300

    fix delivery_zones

--- src/api/delivery_zones.ts
+++ src/api/delivery_zones.ts
@@ -9,8 +9,8 @@ export default class ApiDeliveryZones {
         this.api = api
     }
 
-    list(page: number = 1, limit: number = 15) {
-        return this.api.get(`/claptable/delivery_zones?page=${page}&limit=${limit}`)
+    list(business_id: string, page: number = 1, limit: number = 15) {
+        return this.api.get(`/claptable/delivery_zones/${business_id}?page=${page}&limit=${limit}`)
     }
     view(id: string) {
         return this.api.get(`/claptable/delivery_zones/${id}`)
